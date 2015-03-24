0ecc995a1d9bc153521a4e459fe40e318d334dda
Isha <isha.kumaarr@gmail.com>
2015-03-24 15:18:06 -0700

    Remove @override declarations that were casuing build errors for me

--- src/Implementations/MockQueryManager.java
+++ src/Implementations/MockQueryManager.java
@@ -16,78 +16,65 @@ import Interfaces.IQueryManager;
 
 public class MockQueryManager implements IQueryManager{
 
-	@Override
 	public List<Room> getOccupiedRooms(Calendar date) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public List<Customer> getCurrentCustomers(Calendar date) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public int getAverageStayDuration(Calendar startDate, Calendar endDate) {
 		// TODO Auto-generated method stub
 		return 0;
 	}
 
-	@Override
 	public RoomType getMostPopularRoomType(Calendar startDate, Calendar endDate) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Employee getBestEmployee(Calendar startDate, Calendar endDate) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Employee getWorstEmployee(Calendar startDate, Calendar endDate) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public List<Customer> getValuedCustomers(int reservationNumberThreshold) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public void deleteReservation(Reservation reservationToDelete) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void updateCustomerPassword(Customer customer, String newPassword) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void updateEmployeePassword(Employee employee, String newPassword) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public void updateReservationRoom(Reservation reservation, Room newRoom) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	@Override
 	public Customer getCustomer(String name, String phone_number) {
 		return new Customer(name, phone_number, "password");
 	}
 
-	@Override
 	public List<Reservation> getReservations(Customer customer) {
 		List<Reservation> reservations = new ArrayList<Reservation>();
 		
