fa781ab8ea23bace4add9d9fc2ebe62d25f8c9a6
MacKenzie <mackenzieajudd@gmail.com>
2015-03-24 10:10:36 -0700

    Added interfaces package

---
+++ src/Interfaces/IQueryManager.java
@@ -0,0 +1,34 @@
+package Interfaces;
+import java.util.List;
+
+import data_classes.Customer;
+import data_classes.Employee;
+import data_classes.Reservation;
+import data_classes.Room;
+import data_classes.RoomType;
+
+
+public interface IQueryManager {
+	List<Room> getOccupiedRooms(String date);
+	
+	List<Customer> getCurrentCustomers(String date);
+	
+	int getAverageStayDuration(String startDate, String endDate);
+	
+	RoomType getMostPopularRoomType(String startDate, String endDate);
+	
+	Employee getBestEmployee(String startDate, String endDate);
+	Employee getWorstEmployee(String startDate, String endDate);
+	
+	List<Customer> getValuedCustomers(int reservationNumberThreshold);
+	
+	void deleteReservation(Reservation reservationToDelete);
+	
+	void updateCustomerPassword(Customer customer, String newPassword);
+	void updateEmployeePassword(Employee employee, String newPassword);
+	
+	void updateReservationRoom(Reservation reservation, Room newRoom);
+	
+	//Basic Queries needed
+	Customer getCustomer(String name, String phone_number);
+}
